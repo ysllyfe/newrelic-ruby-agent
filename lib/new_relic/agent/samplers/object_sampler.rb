e75b3c664a8a3bd446f0c86633b7464c326a59a7
Ben Weintraub <ben@newrelic.com>
2013-05-06 11:29:59 -0700

    Patch ObjectSampler to track allocated objects counter as well
    Conflicts:
    	lib/new_relic/agent/samplers/object_sampler.rb

--- lib/new_relic/agent/samplers/object_sampler.rb
+++ lib/new_relic/agent/samplers/object_sampler.rb
@@ -7,18 +7,28 @@ module NewRelic
 
         def initialize
           super :objects
+          @last_allocation_count = ObjectSpace.allocated_objects
         end
 
         def stats
           stats_engine.get_stats_no_scope("GC/objects")
         end
 
+        def allocated_object_stats
+          stats_engine.get_stats_no_scope("GC/object_allocations")
+        end
+
         def self.supported_on_this_platform?
-          defined?(ObjectSpace) && ObjectSpace.respond_to?(:live_objects)
+          defined?(ObjectSpace) && ObjectSpace.respond_to?(:live_objects) && ObjectSpace.respond_to?(:allocated_objects)
         end
 
         def poll
           stats.record_data_point(ObjectSpace.live_objects)
+
+          allocated_objects = ObjectSpace.allocated_objects
+          allocation_delta = allocated_objects - @last_allocation_count
+          allocated_object_stats.record_data_point(allocation_delta)
+          @last_allocation_count = allocated_objects
         end
       end
     end
